6f8b8c1798a988d8207c8f5be77c0bc0826dcf10
altoid <kavulikar@gmail.com>
2025-01-29 17:12:06 +0300

    updated a part of my code

--- cart_action.php
+++ cart_action.php
@@ -1,4 +1,16 @@
 <?php  
 
+include 'config.php';
+session_start();
 
+header('Content-Type: application/json');
+
+$response = ["success" => false , "message" => "invalid request"];
+
+if(!isset($_SESSION['user_id'])){
+    echo json_encode(["success" => false , "message" => "user not logged in"]);
+    exit;
+}
+
+$user_id = $_SESSION['user_id'];
 ?>
