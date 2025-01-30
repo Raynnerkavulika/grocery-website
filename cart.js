b70798a01af2613a7758223569d0322f6372a8c5
altoid <kavulikar@gmail.com>
2025-01-30 14:12:58 +0300

    updated

---
+++ cart.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    //function to delete an item
+    document.querySelectorAll(".delete-item").forEach(btn => {
+        btn.addEventListener("click" , function (e)){
+            e.preventDefault
+        }
+    })
+})
