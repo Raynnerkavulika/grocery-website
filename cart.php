dcc131f06cbdae4cfa06736b5378fbb5a69f0f8e
altoid <kavulikar@gmail.com>
2025-01-30 14:59:17 +0300

    updated

--- cart.php
+++ cart.php
@@ -42,19 +42,19 @@ if(!isset($user_id)){
               if($select_cart->rowCount()>0){
                 while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
             ?>
-                <form action="" method="post" enctype="multipart/form-data">
-                    <a href="cart.php?delete=<?= $fetch_cart['id'];?>" onclick="return confirm('are you sure you want to delete this item from the wishlist?');" class="fas fa-times"></a>
+                <div class="box">
+                   <a href="#" class="fas fa-times delete-item" data-id="<?= $fetch_cart['id']; ?>"></a>
+                    <!-- <a href="cart.php?delete=<?= $fetch_cart['id'];?>" onclick="return confirm('are you sure you want to delete this item from the wishlist?');" class="fas fa-times"></a> -->
                     <a href="view_page.php?pid=<?= $fetch_cart['pid'];?>" class="fas fa-eye"></a>
                     <img src="uploaded_img/<?= $fetch_cart['image'];?>" alt="">
                     <div class="price">sh <?= $fetch_cart['price'];?>/-</div>
                     <div class="name"><?= $fetch_cart['name'];?></div>
-                    <input type="hidden" name="cart_id" value="<?= $fetch_cart['id'];?>">
-                    <div class="flex-btn">
-                      <input type="number" name="p_qty" min="1" value="<?= $fetch_cart['quantity'];?>" class="qty">
-                       <input type="submit" value="update" name="update_quantity" class="option-btn">
-                    </div>
+                    <!-- <input type="hidden" name="cart_id" value="<?= $fetch_cart['id'];?>"> -->
+                      <input type="number" min="1" value="<?= $fetch_cart['quantity']; ?>" class="qty" data-id="<?= $fetch_cart['id']; ?>">
+                      <!-- <input type="number" min="1" name="qty" value="<?= $fetch_cart['quantity'];?>" class="qty"> -->
+                       <!-- <input type="submit" value="update" name="update_quantity" class="option-btn"> -->
                     <div class="sub-total">sub total: <span>Sh <?= $sub_total =  $fetch_cart['price'] * $fetch_cart['quantity'] ?>/-</span></div>
-                </form>
+                </div>
             <?php  
                   $grand_total += $sub_total;
                   }
@@ -64,3 +64,17 @@ if(!isset($user_id)){
             ?>
         </div>
     </section>
+
+    <section class="cart-total">
+        <p>grand total: <span>Sh <?= $grand_total ?>/-</span></p>
+        <a href="shop.php" class="btn">continue shopping</a>
+        <a href="cart.php?delete_all" class="delete-btn <?= ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('are you sure you want to delete all the items from your cart?');">delete all</a>
+        <a href="checkout.php" class="option-btn">proceed to checkout</a>
+    </section>
+
+    <script src="cart.js"></script>
+
+    <?php include 'footer.php';?>
+    
+    </body> 
+    </html>
